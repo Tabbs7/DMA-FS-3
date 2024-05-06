4c0b1c2cfca1f7e96a244a88dd7173319eddcecb
Tabbs7 <turalabbasov0787.@gmail.com>
2024-05-06 16:20:25 +0400

    update

---
+++ task 7/assets/js/script.js
@@ -0,0 +1,74 @@
+// 1.	1-dən 10-a qədər ədədlərin cəmini (hasilini) tapın.
+
+// let cem=0,hasil=1
+// for(let eded=1 ; eded<10; eded++){
+    
+//         cem+=eded;
+//        hasil*=eded
+// }
+// console.log(cem,"cem"); 
+// console.log(hasil,"hasil");
+
+
+
+
+
+// 2.	1-dən100-ə qədər cüt ədədləri / onların cəmini / hasilini tapın
+
+
+// let cem=0, hasil=1
+// for(let eded=1 ; eded<100; eded++) {
+
+// if(eded%2==0){
+//     cem+=eded;
+//     hasil*=eded
+//     console.log(eded,"cuteded");
+// }
+
+// }
+// console.log(cem,"cem");
+// console.log(hasil,"hasil");
+
+
+
+
+// 3.	3 rəqəmli ədədlərin ədədi ortasını tapın
+
+// let cem=0,say=0
+// for(let eded=100 ; eded<1000; eded++) {
+//     cem+=eded;
+// say+=1
+// }
+// console.log(cem/say,"avg");
+
+
+// 4.	3 rəqəmli ədədlərdən 7 və 8-ə bölünən ədədləri tapın
+
+
+// let cem=0;
+//   for(let eded=1 ; eded<=999; eded++) {
+//     if(eded%7==0 && eded%8==0)
+//     cem+=eded;
+// console.log(eded);
+// }
+
+
+// 5.	3 rəqəmli ədədlərdən 7 və 8-ə bölünən ədədlərin cəmini tapın
+
+// let cem=0;
+//   for(let eded=1; eded<=999; eded++){
+//     if(eded%7==0 && eded%8==0)
+//         cem+=eded;
+// }
+//     console.log(cem);
+
+
+// 6.	3 rəqəmli ədədlərdən 7 və 8-ə bölünən ilk ədədi tapın
+
+for(let eded=100; eded<=999; eded++){
+        if(eded%7==0 && eded%8==0)
+            console.log(eded);
+        break;
+    }
+
+    
